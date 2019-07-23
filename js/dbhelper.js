bd9da7ebc5f42609124edd4807c1f8b3f13785e9
Nora Jaara <norajaara@protonmail.com>
2019-07-23 17:22:37 -0400

    minor changes

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
+    //const port = 8000 // Change this to your server port
     //  return `http://localhost:${port}/data/restaurants.json`;
     return `../data/restaurants.json`;
   }
@@ -144,14 +144,14 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return (`../restaurant.html?id=${restaurant.id}`);
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.photograph}`);  // dot added
+    return (`../img/${restaurant.photograph}`);  // dot added
   }
 
   /**
